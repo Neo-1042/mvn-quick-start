27fdf97c86b6e0cf449764362d90f53081830c7a
Neo-1042 <rafaelh.71642@gmail.com>
2025-10-21 23:52:39 -0600

    Add sysout to main(), run application with 'java -cp <jar> <class>'

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,12 +3,13 @@ package clinic.programming.training;
 public class Application {
     
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println ("Inside Application's constructor");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
+    	System.out.println ("Starting Maven Demo Application");
+        System.out.println("Java is cool.");
 	Application app = new Application();
     }
 }
